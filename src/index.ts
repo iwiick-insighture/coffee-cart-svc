5d96923c23122841bd8affc42ebddb719c1023d3
iwiick <135204780+iwiick-insighture@users.noreply.github.com>
2024-03-19 09:22:35 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,11 @@ connectToMongoDB()
         );
         app.use(bodyParser.json());
         app.use(cors());
+        app.use((req, res, next) => {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+        });
         app.use(router);
         app.listen(configData.port, () => {
           listenToOrders();
